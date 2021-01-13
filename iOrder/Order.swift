bbfb369d068f25e3ede486f1920f9758bdff1102
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-13 16:09:46 -0500

    Starting Point

---
+++ iOrder/Order.swift
@@ -0,0 +1,31 @@
+//
+//  Order.swift
+//  iOrder
+//
+//  Created by Iurie Guzun on 2021-01-13.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import SwiftUI
+
+class Order {
+    var items = [MenuItem]()
+
+    var total: Int {
+        if items.count > 0 {
+            return items.reduce(0) { $0 + $1.price }
+        } else {
+            return 0
+        }
+    }
+
+    func add(item: MenuItem) {
+        items.append(item)
+    }
+
+    func remove(item: MenuItem) {
+        if let index = items.firstIndex(of: item) {
+            items.remove(at: index)
+        }
+    }
+}
