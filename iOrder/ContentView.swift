9053cd2267841b9346832519b8263eaecfb49585
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-13 15:55:36 -0500

    Initial Commit

---
+++ iOrder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOrder
+//
+//  Created by Iurie Guzun on 2021-01-13.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
